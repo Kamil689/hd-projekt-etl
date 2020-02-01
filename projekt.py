6844eee64ffb0c6fe497b55e5d1ff39fe30dc032
Kamil689 <kamilkoterla@gmail.com>
2020-02-01 13:19:28 +0100

    refactor the code - split redundant code into separate functions - ask whether to continue after each step

--- projekt.py
+++ projekt.py
@@ -22,10 +22,82 @@ def ask_for_choice():
         print("This is not a number.")
 
 
+def ask_for_continue():
+    input_continue = input('Want to continue Y/N?: ').upper()
+    if input_continue == 'Y':
+        switch_case()
+    elif input_continue == 'N':
+        pass
+    else:
+        print('Invalid choice.')
+        ask_for_continue()
+
+
 containers = set()
 brand = []
 product_name = []
 shipping = []
+
+
+def open_db_connection():
+    # open config file and assign variables
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    db = config['mysql']['db']
+    db_user = config['mysql']['user']
+    db_password = config['mysql']['password']
+
+    # connect to MariaDB
+    mariadb_connection = mariadb.connect(user=db_user, password=db_password, database=db)
+    cursor = mariadb_connection.cursor()
+    return mariadb_connection, cursor
+
+
+def extract():
+    global containers
+
+    # open connection
+    u_client = uReq(my_url)
+    page_html = u_client.read()
+    u_client.close()
+
+    # html parsing
+    page_soup = soup(page_html, "html.parser")
+
+    # grabs each product
+    containers = page_soup.find_all("div", {"class": "item-container"})
+
+
+def transform():
+    # get info for each product
+    for index, container in enumerate(containers, start=0):
+        brand_container = container.findAll("div", {"class": "item-info"})
+        brand.append(brand_container[0].div.a.img["title"])
+
+        title_container = container.findAll("a", {"class": "item-title"})
+        product_name.append(title_container[0].text)
+
+        shipping_container = container.findAll("li", {"class": "price-ship"})
+        shipping.append(shipping_container[0].text.strip())
+
+
+def load():
+    mariadb_connection, cursor = open_db_connection()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS etl ("
+                   "brand varchar(225),"
+                   "product_name varchar(225),"
+                   "shipping varchar(255)"
+                   ");")
+
+    for index, container in enumerate(containers, start=0):
+        cursor.execute("INSERT INTO etl (brand, product_name, shipping) VALUES (%s,%s,%s)",
+                       (brand[index], product_name[index].replace(",", "|"), shipping[index]))
+        mariadb_connection.commit()
+
+    brand.clear()
+    product_name.clear()
+    shipping.clear()
 
 
 def switch_case():
@@ -37,158 +109,60 @@ def switch_case():
 
     # Extract
     if input_choice == 1:
-        # open connection
-        u_client = uReq(my_url)
-        page_html = u_client.read()
-        u_client.close()
-
-        # html parsing
-        page_soup = soup(page_html, "html.parser")
-
-        # grabs each product
-        containers = page_soup.find_all("div", {"class": "item-container"})
-
-        # ask for continue
-        input_continue = input('Data Exctracted, want to continue Y/N?: ').upper()
-        if input_continue == 'Y':
-            switch_case()
+        extract()
+        print('Data extracted.')
+        ask_for_continue()
 
     # Transform
     elif input_choice == 2:
         if containers != set():
             try:
-                # get info for each product
-                for index, container in enumerate(containers, start=0):
-                    brand_container = container.findAll("div", {"class": "item-info"})
-                    brand.append(brand_container[0].div.a.img["title"])
-
-                    title_container = container.findAll("a", {"class": "item-title"})
-                    product_name.append(title_container[0].text)
-
-                    shipping_container = container.findAll("li", {"class": "price-ship"})
-                    shipping.append(shipping_container[0].text.strip())
-
-                # ask for continue
-                input_continue = input('want to continue Y/N?: ').upper()
-                if input_continue == 'Y':
-                    switch_case()
+                transform()
+                print('Data transformed.')
+                ask_for_continue()
             except NameError:
                 print('First u need to run step 1.')
-                input_choice = ask_for_choice()
+                switch_case()
         else:
             print('First u need run step 1.')
+            switch_case()
 
     # Load
     elif input_choice == 3:
-        if (brand == [] or shipping == [] or product_name == []):
+        if brand == [] or shipping == [] or product_name == []:
             print('First u need step 1 next 2 to enter this step (3)')
+            switch_case()
         else:
             try:
-                # open config file and assign variables
-                config = configparser.ConfigParser()
-                config.read("config.ini")
-                db = config['mysql']['db']
-                db_user = config['mysql']['user']
-                db_password = config['mysql']['password']
-
-                # connect to MariaDB
-                mariadb_connection = mariadb.connect(user=db_user, password=db_password, database=db)
-                cursor = mariadb_connection.cursor()
-
-                cursor.execute("CREATE TABLE IF NOT EXISTS etl ("
-                               "brand varchar(225),"
-                               "product_name varchar(225),"
-                               "shipping varchar(255)"
-                               ");")
-
-                for index, container in enumerate(containers, start=0):
-                    cursor.execute("INSERT INTO etl (brand, product_name, shipping) VALUES (%s,%s,%s)",
-                                   (brand[index], product_name[index].replace(",", "|"), shipping[index]))
-                    mariadb_connection.commit()
-
-                brand.clear()
-                product_name.clear()
-                shipping.clear()
-                print("Data was loaded.")
+                load()
+                print('Data loaded to database.')
+                ask_for_continue()
             except NameError:
                 print('First u need to run step 1 and step 2.')
                 switch_case()
 
     # do all etl
     elif input_choice == 4:
-        # open connection
-        u_client = uReq(my_url)
-        page_html = u_client.read()
-        u_client.close()
-
-        # html parsing
-        page_soup = soup(page_html, "html.parser")
-
-        # grabs each product
-        containers = page_soup.find_all("div", {"class": "item-container"})
-
-        # open config file and assign variables
-        config = configparser.ConfigParser()
-        config.read("config.ini")
-        db = config['mysql']['db']
-        db_user = config['mysql']['user']
-        db_password = config['mysql']['password']
-
-        # connect to MariaDB
-        mariadb_connection = mariadb.connect(user=db_user, password=db_password, database=db)
-        cursor = mariadb_connection.cursor()
-
-        cursor.execute("CREATE TABLE IF NOT EXISTS etl ("
-                       "brand varchar(225),"
-                       "product_name varchar(225),"
-                       "shipping varchar(255)"
-                       ");")
-
-        # get info for each product
-        for index, container in enumerate(containers, start=0):
-            brand_container = container.findAll("div", {"class": "item-info"})
-            brand.append(brand_container[0].div.a.img["title"])
-
-            title_container = container.findAll("a", {"class": "item-title"})
-            product_name.append(title_container[0].text)
-
-            shipping_container = container.findAll("li", {"class": "price-ship"})
-            shipping.append(shipping_container[0].text.strip())
-
-        for index, container in enumerate(containers, start=0):
-            cursor.execute("INSERT INTO etl (brand, product_name, shipping) VALUES (%s,%s,%s)",
-                           (brand[index], product_name[index].replace(",", "|"), shipping[index]))
-            mariadb_connection.commit()
-
-        brand.clear()
-        product_name.clear()
-        shipping.clear()
-        print("Data was loaded.")
+        extract()
+        print('Data extracted.')
+        transform()
+        print('Data transformed.')
+        load()
+        print('Data loaded to database.')
+        ask_for_continue()
 
     # erase data
     elif input_choice == 5:
-        # open config file and assign variables
-        config = configparser.ConfigParser()
-        config.read("config.ini")
-        db = config['mysql']['db']
-        db_user = config['mysql']['user']
-        db_password = config['mysql']['password']
-
-        # connect to MariaDB
-        mariadb_connection = mariadb.connect(user=db_user, password=db_password, database=db)
-        cursor = mariadb_connection.cursor()
+        mariadb_connection, cursor = open_db_connection()
 
         cursor.execute("DROP TABLE IF EXISTS etl;")
         mariadb_connection.commit()
-        print('Data Erased.')
-
-        # ask for continue
-        input_continue = input('want to continue Y/N?: ').upper()
-        if input_continue == 'Y':
-            switch_case()
+        print('Data erased.')
+        ask_for_continue()
 
     else:
         print('U can only choose from options 1 to 5.')
+        switch_case()
 
 
 # run
